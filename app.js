7e98d84249b69cc867562d0b2eb518bfa1c8c718
FranFi15 <franciscofilippa1995@gmail.com>
2023-06-07 17:26:49 -0300

    portafolio para web terminado

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const toggleMenu = () => document.body.classList.toggle("open");
+
+const seccionesOcultas = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("mostrar", entry.isIntersecting);
+    });
+  },
+  { threshold: 0.5 }
+);
+
+seccionesOcultas.forEach((seccion) => observer.observe(seccion));
